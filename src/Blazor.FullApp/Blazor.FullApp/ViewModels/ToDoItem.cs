f2e2ccb274fe47a659938a0b64c952a7d43968af
Alex Almeida <alex.almeida@farfetch.com>
2024-03-04 16:08:25 -0300

    feat: Create view model to handle to-do list in the home page
    Signed-off-by: Alex Almeida <alex.almeida@farfetch.com>

---
+++ src/Blazor.FullApp/Blazor.FullApp/ViewModels/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor.FullApp.ViewModels;
+
+public class ToDoItem
+{
+    public  bool IsDone { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime? DoneDate { get; set; }
+
+    public string Tag { get; set; }
+}
